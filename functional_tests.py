829d22f4b31542ac6718a89edcdc60ffa848647f
Bjørnar Hansen <tilbjornar@gmail.com>
2016-03-01 20:10:27 +0100

    Adds functional tests for checking we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -17,17 +18,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item …'
+        )
 
         # She types Buy peackcock feathers" into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page
-        # lists "1: Buy peacock feathers" an an item in a to-do list
+        # lists "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # There is still a text box inviting her to add another item.
         # She enters "Use feathers to make a fly"
+        # inputbox.send_keys('Use feathers to make a fly')
 
         # The page updates again, and now shows both items on her
         # list.
@@ -37,6 +53,7 @@ class NewVisitorTest(unittest.TestCase):
         # explanatory text to that effect.
 
         # She visits that URL - her to-do list is still there.
+        self.fail('Finish the test!')
 
         # Satisfied, she goes back to sleep
 
